e8268fde3a12bc7077ba2eb7937f950ca7431345
Vincent Wei <vincent.c.wei@gmail.com>
2022-01-14 16:22:58 -0800

    Added lamp sprite + mulitple light flickering

--- Assets/Scripts/Object/IsFlickering.cs
+++ Assets/Scripts/Object/IsFlickering.cs
@@ -10,32 +10,38 @@ public class IsFlickering : MonoBehaviour
     public float OffInterval;
     public float RandomnessOffset;
     private float flicker_time;
+    private bool on;
     private IsFlickering myIsFlickering;
-    private Light2D myLight; 
+    private Light2D[] myLights; 
     // Start is called before the first frame update
     void Start()
     {
         myIsFlickering = gameObject.GetComponent<IsFlickering>();
-        myLight = GetComponentInChildren<Light2D>();
+        myLights = GetComponentsInChildren<Light2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        on = myLights[0].intensity != 0;
         if (Time.time >= flicker_time)
         {
-            if (myLight.intensity > 0)
+            if (on)
             {
-                myLight.intensity = 0;
-                flicker_time += OffInterval + Random.Range(-1 * RandomnessFactor, RandomnessFactor) + RandomnessOffset;
+                foreach (Light2D l in myLights)
+                {
+                    l.intensity = 0;
+                    flicker_time += OffInterval + Random.Range(-1 * RandomnessFactor, RandomnessFactor) + RandomnessOffset;
+                }
             }
             else
             {
-                myLight.intensity = 1;
-                flicker_time += OnInterval + Random.Range(-1 * RandomnessFactor, RandomnessFactor) + RandomnessOffset;
+                foreach (Light2D l in myLights)
+                {
+                    l.intensity = 1;
+                    flicker_time += OnInterval + Random.Range(-1 * RandomnessFactor, RandomnessFactor) + RandomnessOffset;
+                }
             }
-            
         }
-        
     }
 }
