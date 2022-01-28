69701e505a9720032d11b916650cf099e8b69663
Poseidis <vincent.c.wei@gmail.com>
2022-01-27 21:57:16 -0600

    Able to define "Scenes" for which the Camera will follow
    Scene objects determine different camera tracking behavior for a given readion. They must added to the list of scenes under the main camera, and are defined by min and max x and y values, as well as the type of Scene (in an enum) and it's activity status. Inactive scenes are ignored.

--- Assets/CameraTrackPlayerByScene.cs
+++ Assets/CameraTrackPlayerByScene.cs
@@ -9,28 +9,39 @@ public class CameraTrackPlayerByScene : MonoBehaviour
     private bool needtomovex;
     private bool needtomovey;
 
-    public struct Scene
+    public class Scene
     {
-        public Scene(float xmin, float xmax, float ymin, float ymax, SceneType t)
+        public Scene(float xmin, float xmax, float ymin, float ymax, SceneType t, bool a)
         {
             xMin = xmin;
             yMin = ymin;
             xMax = xmax;
             yMax = ymax;
             type = t;
+            active = a;
         }
+
+        public Scene()
+        {
+            xMin = yMin = xMax = yMax = float.NaN;
+            type = SceneType.DEFAULT;
+            active = false;
+        }
+
         public float xMin;
         public float yMin;
         public float xMax;
         public float yMax;
         public SceneType type;
+        public bool active;
     }
     public enum SceneType
     {
         OneScreen,
         ScrollHorizontal,
         ScrollVertical,
-        ScrollOmni
+        ScrollOmni,
+        DEFAULT
     }
 
     [SerializeField] private List<Scene> scenes;
@@ -42,14 +53,69 @@ public class CameraTrackPlayerByScene : MonoBehaviour
         cam = FindObjectOfType<Camera>();
         ploc = FindObjectOfType<Player>().transform;
         //if (scenes.Count > 0) curScene = scenes[0];
-        curScene = new Scene(-7.094f, 15.094f, 0, float.PositiveInfinity, SceneType.ScrollOmni);
+        scenes = new List<Scene>();
+        //Debug.Log(cam.orthographicSize);
+        //Debug.Log(cam.orthographicSize * cam.aspect);
+        Scene scene1 = new Scene(-7.094f, 15.094f, 0, float.PositiveInfinity, SceneType.ScrollOmni, true);
+        scenes.Add(scene1);
+        Scene scene2 = new Scene(15.094f + 2f * cam.orthographicSize * cam.aspect, 30.094f, 0, float.PositiveInfinity, SceneType.ScrollOmni, true);
+        scenes.Add(scene2);
+
+        curScene = scenes[0];
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if player has left this scene, change scenes to the scene the player is now in
+        if (!curScene.active ||
+            ploc.position.x > curScene.xMax + cam.orthographicSize * cam.aspect ||
+            ploc.position.x < curScene.xMin - cam.orthographicSize * cam.aspect ||
+            ploc.position.y > curScene.yMax + cam.orthographicSize ||
+            ploc.position.y < curScene.yMin - cam.orthographicSize)
+        {
+            Scene temp = FindCurrentScene();
+            if (temp.active)
+            {
+                curScene = temp;
+            }
+        }
+        MoveCamera(curScene);
+    }
+
+    private Scene FindCurrentScene()
+    {
+        foreach (Scene s in scenes)
+        {
+            if (ploc.position.x <= s.xMax + cam.orthographicSize * cam.aspect &&
+                ploc.position.x >= s.xMin - cam.orthographicSize * cam.aspect &&
+                ploc.position.y <= s.yMax + cam.orthographicSize &&
+                ploc.position.y >= s.yMin - cam.orthographicSize)
+            {
+                needtomovex = needtomovey = true;
+                return s;
+            }
+        }
+        return new Scene();
+    }
+
+    private void MoveCamera(Scene curScene)
+    {
         switch (curScene.type)
         {
+            case SceneType.OneScreen:
+                if (needtomovex)
+                {
+                    SetCameraX(curScene.xMin);
+                    needtomovex = false;
+                }
+                if (needtomovey)
+                {
+                    SetCameraY(curScene.yMin);
+                    needtomovey = false;
+                }
+                break;
+
             case SceneType.ScrollHorizontal:
                 if (ploc.position.x < curScene.xMin)
                 {
